48a90395b639c1c209ecd56bde62cc59b96494f4
davyatze01 <d.atzeni20@studenti.unica.it>
2025-10-24 12:25:08 +0200

    Bus routing with r5py

--- bus_routing.py
+++ bus_routing.py
@@ -1,8 +1,10 @@
 import r5py
 import geopandas
-import geopandas
 import shapely
 import datetime
+import folium
+import folium.plugins
+import pandas
 
 # Recupero la path del pbf di Cagliari e del GTFS di Ctm
 CA_PATH = "pbf_files/cagliari-latest.osmv2.pbf"
@@ -10,9 +12,9 @@ GTFS_PATH = "gtfs/GTFS.zip"
 
 # Prendo due posizioni
 
-# Origine A = 9.110754, 39.235687 
+ORIGIN = shapely.Point(9.110754, 39.235687 )
 
-# Destinazione B = 9.1235628871721, 39.22348159436033
+DESTINATION = shapely.Point(9.1235628871721, 39.22348159436033)
 
 # Creo la transport network dei bus di Cagliari
 # utilizzando il gtfs fornito da CTM Spa
@@ -33,7 +35,7 @@ origins = geopandas.GeoDataFrame(
     {
         "id": [1],
         "geometry": [
-            shapely.Point(9.110754, 39.235687)
+            ORIGIN
         ],
     },
     crs="EPSG:4326",
@@ -43,9 +45,9 @@ origins = geopandas.GeoDataFrame(
 
 destinations = geopandas.GeoDataFrame(
     {
-        "id": [1],
+        "id": [2],
         "geometry": [
-            shapely.Point(9.123562, 39.223481)
+            DESTINATION
         ],
     },
     crs="EPSG:4326",
@@ -57,12 +59,65 @@ travel_times = r5py.TravelTimeMatrix(
     ctm_network_cagliari,
     origins=origins,
     destinations=destinations,
-    departure = datetime.datetime(2025, 10, 22, 10, 22),
+    departure = datetime.datetime(2025, 10, 27, 10, 22),
     transport_modes = [
-        r5py.TransportMode.BUS,
-        r5py.TransportMode.WALK,
+        r5py.TransportMode.BUS
     ],
     snap_to_network= True,
 )
 
+detailed_itineraries = r5py.DetailedItineraries(
+    ctm_network_cagliari,
+    origins=origins,
+    destinations=destinations,
+    departure= datetime.datetime(2025, 10, 27, 10, 22),
+    transport_modes=[r5py.TransportMode.BUS],
+    snap_to_network=True,
+)
+
 print(travel_times)
+
+print("------------------------")
+
+print(detailed_itineraries)
+
+print("-----------------------------------")
+
+detailed_itineraries["mode"] = detailed_itineraries.transport_mode.astype(str)
+detailed_itineraries["travel time (min)"] = detailed_itineraries.travel_time.apply(
+    lambda t: round(t.total_seconds() / 60.0, 2)
+)
+detailed_itineraries["trip"] = detailed_itineraries.apply(
+    lambda row: f"{row.from_id} → destination",
+    axis=1
+)
+
+detailed_routes_map = (
+    detailed_itineraries[
+        [
+            "geometry",
+            "distance",
+            "mode",
+            "travel time (min)",
+            "from_id",
+            "to_id",
+            "trip",
+            "option",
+            "segment",
+        ]
+    ]
+    .explore(
+        tooltip=["trip", "option", "segment", "mode", "travel time (min)", "distance"],
+        column="mode",
+        tiles="CartoDB.Positron",
+        style_kwds={
+            "weight": 3,
+            "opacity": 0.8,
+        },
+        highlight_kwds={
+            "weight": 6,
+            "opacity": 1,
+        },
+
+    )
+)
