a53d5f91946e0fd1aba89c72b013ed6e296f7298
davyatze01 <davideatzeni.unica@gmail.com>
2025-10-29 18:01:40 +0100

    #

--- otp2.py
+++ otp2.py
@@ -6,23 +6,22 @@ OTP_URL = "http://localhost:8080/otp/routers/default/index/graphql"
 query = """
 query {
   plan(
-    fromPlace: "39.22294897283518,9.114625009108789",
-    toPlace: "39.23998019279815,9.098612322838331",
-    numItineraries: 1,
-    transportModes: [
-      {
-        mode: BUS
-      }
-    ]
-  ) {
+  fromPlace: "39.22294897283518,9.114625009108789",
+  toPlace: "39.23998019279815,9.098612322838331",
+  numItineraries: 1,
+  transportModes: [{ mode: BUS }],
+  date: "2025-10-29",
+  time: "12:00:00"
+) {
     itineraries {
-      legs {
+        legs {
         mode
-        from {lat lon}
-        to {lat lon}
-      }
+        from { lat lon }
+        to { lat lon }
+        }
     }
-  }
+   }
+
 }
 """
 
