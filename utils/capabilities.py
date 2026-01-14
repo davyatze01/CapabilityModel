aeeb9faac7c6236c9fde161da8aac5630b1b7e50
Matteo Mocci <moccimatteo23@gmail.com>
2026-01-14 10:27:12 +0100

    Added capability loop for all nodes

--- utils/capabilities.py
+++ utils/capabilities.py
@@ -14,24 +14,3 @@ def cap_eat(S):
             return 0.6
     else:
         return 0.4
-
-def test():
-    dining_out_accessibility = []
-    on_the_go_accessibility = []
-    services = []
-    origin = (39.22231439353061, 9.113848879825527)
-
-    for type in dining_out_list:
-        dining_out_accessibility.append(delta_g.accessibility(type, origin))
-    print("dining_out_accessibility:", dining_out_accessibility)
-    services.append(choquet_integral(dining_out_accessibility, cap_dining_out))
-    print("Dining out service:", services[-1])
-
-    for type in on_the_go_list:
-        on_the_go_accessibility.append(delta_g.accessibility(type, origin))
-    print("on_the_go_accessibility:", on_the_go_accessibility)
-    services.append(choquet_integral(on_the_go_accessibility, cap_on_the_go))
-    print("On the go service:", services[-1])
-
-    capability_to_eat = choquet_integral(services, cap_eat)
-    print("Capability to eat:", capability_to_eat)
